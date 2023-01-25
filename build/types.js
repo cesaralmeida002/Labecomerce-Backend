c78a9929d4e3042f726557dcb205204792052ba2
cesaralmeida002 <cesaralmeida002@gmail.com>
2023-01-25 10:53:09 -0300

    Labecommerce-exercício-typscript-2

--- build/types.js
+++ build/types.js
@@ -1,3 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.MusicalStyle = void 0;
+var MusicalStyle;
+(function (MusicalStyle) {
+    MusicalStyle["ROCK"] = "ROCK";
+    MusicalStyle["FORRO"] = "FORRO";
+    MusicalStyle["JAZZ"] = "JAZZ";
+})(MusicalStyle = exports.MusicalStyle || (exports.MusicalStyle = {}));
 //# sourceMappingURL=types.js.map
