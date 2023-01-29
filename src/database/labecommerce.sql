374afa3dfb9e67035f2d6eaf81ac996b9baa5b4e
cesaralmeida002 <cesaralmeida002@gmail.com>
2023-01-29 12:29:59 -0300

    Exercício aprofundamento-SQL

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -29,4 +29,43 @@ VALUES
 (5, "cd-Audio-Slave", 49.90, "MusicalStyle.ROCK"),
 (6, "cd-Bartô-Galeno", 9.90, "MusicalStyle.FORRO");
 
+/*Aprofundamento-SQL*/
+SELECT * FROM users;
 SELECT * FROM product;
+SELECT * FROM product
+WHERE name LIKE "%Metalica";
+
+
+INSERT INTO users (id, email, password)
+VALUES
+(4, "Maju", "Maju123");
+INSERT INTO product (id, name, price, category)
+VALUES
+(7, "cd-soft-Jazz", 59.90, "MusicalStyle.JAZZ");
+
+SELECT * FROM users
+ORDER BY id ASC;
+
+SELECT id FROM users;
+
+SELECT COUNT(*) as IDS,
+	id
+	FROM users
+	GROUP BY id;
+
+DELETE FROM users 
+ WHERE id = 4;
+
+DELETE FROM product 
+ WHERE id = 4;
+
+ -- renomeando uma tabela
+UPDATE users
+ SET  id = '2'
+ WHERE email = 'mariazinha@email.com';
+UPDATE product
+ SET  name = 'cd-Soft-JAZZ'
+ WHERE name = 'JAZZZZZZ' ;
+
+ SELECT email FROM product, 
+  AND ORDER BY email DESC;
