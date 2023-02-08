a211226016a4c39dd46f105987333f2ce5fd31a3
cesaralmeida002 <cesaralmeida002@gmail.com>
2023-02-08 15:59:57 -0300

    exercício relações-SQL-1

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,25 +1,31 @@
--- Active: 1674926831045@@127.0.0.1@3306
+-- Active: 1675878877832@@127.0.0.1@3306
 CREATE TABLE users (
 	id TEXT PRIMARY KEY UNIQUE NOT NULL,
 	email TEXT NOT NULL,
 	password TEXT NOT NULL
 );
-
-SELECT * FROM users;
-INSERT INTO users (id, email, password)
-VALUES 
-       (1, "jose@email.com", "jose123"),
-       (2, "maria@email.com", "maria123"),
-       (3, "joao@email.com", "joao123");
-
-SELECT * FROM users; 
-
 CREATE TABLE product (
 	id TEXT PRIMARY KEY UNIQUE NOT NULL,
 	name TEXT NOT NULL,
 	price REAL NOT NULL,
     category TEXT NOT NULL 
 );
+CREATE TABLE purchase (
+	id TEXT PRIMARY KEY UNIQUE NOT NULL,
+	total_price REAL NOT NULL,
+	paid INTEGER NOT NULL,
+    delivered_at TEXT, 
+	buyer_id TEXT NOT NULL,
+	 Foreign Key (buyer_id) REFERENCES users (id)
+);
+SELECT * FROM users;
+
+INSERT INTO users (id, email, password)
+VALUES 
+       ("u001", "jose@email.com", "jose123"),
+       ("u002", "maria@email.com", "maria123"),
+       ("u003", "joao@email.com", "joao123"),
+       ("u004", "juca@email.com", "juca123");
 
 INSERT INTO product (id, name, price, category)
 VALUES
@@ -29,12 +35,19 @@ VALUES
 (5, "cd-Audio-Slave", 49.90, "MusicalStyle.ROCK"),
 (6, "cd-Bartô-Galeno", 9.90, "MusicalStyle.FORRO");
 
+INSERT INTO purchase (id, total_price, paid, delivered_at, buyer_id)
+VALUES 
+       ("p001", 25,90, "0", "u001"),
+       ("p002", 37,40, "0", "u002"),
+       ("p003", 54,90, "0", "u003"),
+       ("p004", 22,80, "0", "u004");
+
+
 /*Aprofundamento-SQL*/
 SELECT * FROM users;
 SELECT * FROM product;
 SELECT * FROM product
 WHERE name LIKE "%Metalica";
-
 
 INSERT INTO users (id, email, password)
 VALUES
@@ -69,3 +82,7 @@ UPDATE product
 
  SELECT email FROM product, 
   AND ORDER BY email DESC;
+
+  SELECT * FROM users
+  INNER JOIN purchase
+  ON purchase.buyer_id = users.id
